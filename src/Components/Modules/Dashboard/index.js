20e58f23698a30a048ddd9ea23654057b6d73104
Yornan0325 <jhornan.coningenieria@gmail.com>
2021-09-25 14:19:55 -0500

    instalacion y config de bulma

--- src/Components/Modules/Dashboard/index.js
+++ src/Components/Modules/Dashboard/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Dashboard = () => (
-    <div>Dashboard Module</div>
+    <section class="section">
+    <div class="container">
+      <h1 class="title">
+        Hello World
+      </h1>
+      <p class="subtitle">
+        My first website with <strong>Bulma</strong>!
+      </p>
+    </div>
+  </section>
 );
 
 export default {
