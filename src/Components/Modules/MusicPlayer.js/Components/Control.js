ed653f3e939a483ceea95ce5355ff842fc16e10b
Yornan0325 <jhornan.coningenieria@gmail.com>
2021-10-05 14:38:58 -0500

    image, video, action of play list

---
+++ src/Components/Modules/MusicPlayer.js/Components/Control.js
@@ -0,0 +1,22 @@
+import { faBackward, faForward, faPause, faPlay } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+
+const Control = (props) => {
+    return (
+
+        <div className="control">
+            <button className="control__skipBtn" onClick={()=>props.skipSong(false)}>
+                <FontAwesomeIcon icon={faBackward} />
+            </button>
+            <button className="control__PlayButton" onClick={()=>props.setIsplaying(!props.isPlaying)}>
+                <FontAwesomeIcon icon={props.isPlaying ? faPause : faPlay} />
+            </button>
+            <button className="control__skipBtn" onClick={()=>props.skipSong(true)}>
+                <FontAwesomeIcon icon={faForward} />
+            </button>
+        </div>
+
+    )
+}
+export default Control
